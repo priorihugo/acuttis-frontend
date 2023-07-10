25250b0205e12bf55a84323176dc6a44153d3b4f
Hugo Amoglia Priori <priori.hugo@estudante.ufjf.br>
2023-07-10 18:20:08 -0300

    fix o fix das imagens

--- src/components/ParallaxImage/index.jsx
+++ src/components/ParallaxImage/index.jsx
@@ -3,7 +3,7 @@ import escritorio from '../../assets/images/escritorio.jpg'
 
 function ParallaxImage() {
   return (
-    <div style={{width: '100%'}}className="parallax-container app-parallax-container">
+    <div className="parallax-container app-parallax-container">
       <div style={{width: '100%' , backgroundSize: 'cover' , backgroundRepeat: 'no-repeat' ,  }} className="parallax">
         <img style={{width: '100%'}} src={escritorio} />
       </div>
